6b6c113a5f3bd72a27461583c9ee69b81a4ec796
NGOVINDARAJU <158595722+NGOVINDARAJU@users.noreply.github.com>
2025-09-22 20:28:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Hello from Node.js CI/CD Demo App!");
+  res.send("🚀 Hello from Node.js CI/CD Demo App! by NUNAVATHU Govinda Raju");
 });
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
 });
+
